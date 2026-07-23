0e4d85ae4c4154b633d16669f007ba33978fc344
mayssazitouni <mayssazitouni21@gmail.com>
2026-07-23 14:14:42 +0100

    implement database setup and much more

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { remotePatterns: [
-          { protocol: 'https', hostname: 'covers.openlibrary.org'}]}
+          { protocol: 'https', hostname: 'covers.openlibrary.org'},
+          { protocol: 'https', hostname: '*.public.blob.vercel-storage.com'},
+      ]}
 };
 
 export default nextConfig;
