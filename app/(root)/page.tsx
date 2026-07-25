6ae5f34d0c89c548a570658b991e3c1a6acfc129
mayssazitouni <mayssazitouni21@gmail.com>
2026-07-25 12:27:48 +0100

    coderabbit suggested fixes + cache changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,12 @@ import Hero from '@/components/Hero';
 import BookCard from '@/components/BookCard';
 import {getAllBooks} from "@/lib/actions/book.actions";
 
+
+export const dynamic = 'force-dynamic';
 const Page = async () => {
     const bookResults = await getAllBooks()
     const books = bookResults.success ? bookResults.data ?? [] : []
+
 
 
     return (
